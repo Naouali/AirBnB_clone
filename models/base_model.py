6d29d33c8ace295f2d5211b34e2f0979b7e38c5c
ahmedomi <ahmedomarmiledi@gmail.com>
2020-02-18 08:49:12 +0000

    correct requriement file updating comment

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Base model
+AirBnB project class of base model
 '''
 import uuid
 import datetime
@@ -8,18 +8,22 @@ import datetime
 
 class BaseModel:
     def __init__(self):
+        ''' constructor of class model '''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+        ''' str function '''
         return "[{}] ({}) {}". \
                 format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
+        ''' save update datetime '''
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        ''' new dict '''
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = type(self).__name__
         new_dict["created_at"] = new_dict["created_at"].isoformat()
