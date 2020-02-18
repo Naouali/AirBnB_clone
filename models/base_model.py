1f4a39c5252a8cda3a6e7990a40fc82bf7e9edd3
ahmedomi <ahmedomarmiledi@gmail.com>
2020-02-18 10:12:33 +0000

    updating task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ constructor of class model """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs is not None:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__ = kwargs.created_at
+                elif key == "updated_at":
+                    self.__dict__ = kwargs.updated_at
+                else:
+                    self.__dict__ = kwargs.key
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ str function """
